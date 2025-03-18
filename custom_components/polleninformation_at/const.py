ebb29926ef48da0dabc47ef9a7158acf914f9ca5
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2025-03-18 08:10:53 +0100

    Cleanups and fixed SCAN_INTERVAL
    * Added options flow and reset integration after config change
    * Removed configurable timeout in favor of fixed SCAN_INTERVAL of 6 hours
    * Moved constants to const.py and use them everywhere
    * Bumped minor version

--- custom_components/polleninformation_at/const.py
+++ custom_components/polleninformation_at/const.py
@@ -1,3 +1,10 @@
 DOMAIN = "polleninformation_at"
+INTEGRATION_NAME = "Polleninformation.at"
+
+DEFAULT_INTERVAL = 360 # 6 hours
+MIN_INTERVAL = 60
+
+CONF_LOCATION = "location"
 CONF_INTERVAL = "interval"
+
 ICON_FLOWER_POLLEN = "mdi:flower-pollen"
--- custom_components/polleninformation_at/sensor.py
+++ custom_components/polleninformation_at/sensor.py
@@ -1,20 +1,14 @@
 import logging
 import voluptuous as vol
+from datetime import timedelta
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity, SensorStateClass
 from homeassistant.const import CONF_NAME, STATE_UNKNOWN
 import homeassistant.helpers.config_validation as cv
-from .const import DOMAIN, CONF_INTERVAL, ICON_FLOWER_POLLEN
+from .const import DOMAIN, INTEGRATION_NAME, DEFAULT_INTERVAL, CONF_INTERVAL, CONF_LOCATION, ICON_FLOWER_POLLEN
 from .api import PollenAPI
 
-_LOGGER = logging.getLogger(__name__)
-
-CONF_LOCATION = "location"
-
-DEFAULT_NAME = "Polleninformation.at"
-DEFAULT_INTERVAL = 21600  # 6 Hours
-
-POLLEN_TYPES = {
+POLLEN_TYPES={
     "alternaria": 23,
     "ambrosia": 6,
     "cupressaceae": 17,
@@ -30,58 +24,78 @@ POLLEN_TYPES = {
     "artemisia": 7
 }
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+SCAN_INTERVAL=timedelta(minutes=DEFAULT_INTERVAL)
+
+PLATFORM_SCHEMA=PLATFORM_SCHEMA.extend({
     vol.Required(CONF_LOCATION): cv.string,
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_INTERVAL, default=DEFAULT_INTERVAL): cv.positive_int,
+    vol.Optional(CONF_NAME, default=INTEGRATION_NAME): cv.string
 })
 
-async def async_setup_platform(hass, config, add_entities, discovery_info=None):
-    """Setup sensor platform for Polleninformation.at."""
-    location = config[CONF_LOCATION]
-    name = config[CONF_NAME]
-    interval = config[CONF_INTERVAL]
+_LOGGER=logging.getLogger(__name__)
 
-    sensors = []
+async def async_setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up platform."""
+
+    name=config[CONF_NAME]
+    location=config[CONF_LOCATION]
+
+    _LOGGER.debug(f"Setup platform: name={name}, location={location}")
+
+    sensors=[]
     for pollen_type, poll_id in POLLEN_TYPES.items():
-        sensors.append(PollenSensor(hass, name, location, interval, poll_id, f"{name}_{pollen_type}"))
+        sensors.append(PollenSensor(hass, name, location, poll_id, f"{name}_{pollen_type}"))
 
     add_entities(sensors, True)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up Polleninformation.at sensor from a config entry."""
-    _LOGGER.debug("Setting up Polleninformation.at sensor from config entry")
-    interval = config_entry.data[CONF_INTERVAL]
+    """Set up sensor."""
+    
+    name=config_entry.data.get(CONF_NAME, INTEGRATION_NAME)
+    location=config_entry.data.get(CONF_LOCATION)
 
-    sensors = []
+    _LOGGER.debug(f"Setup entry: name={name}, location={location}")
+
+    sensors=[]
     for pollen_type, poll_id in POLLEN_TYPES.items():
-        sensors.append(PollenSensor(hass, "Polleninformation.at", "home", interval, poll_id, f"polleninformation_at_{pollen_type}"))
+        sensors.append(PollenSensor(hass, name, location, poll_id, f"{DOMAIN}_{pollen_type}"))
 
     async_add_entities(sensors, True)
+    
+    # Set up config change listener
+    config_entry.async_on_unload(
+        config_entry.add_update_listener(async_update_options)
+    )
+
+async def async_update_options(hass, config_entry):
+    """React to config updates"""
+    _LOGGER.info("Config entry options changed, reloading integration...")
+    await hass.config_entries.async_reload(config_entry.entry_id)
 
 class PollenSensor(SensorEntity):
     """Polleninformation.at Sensor."""
 
-    def __init__(self, hass, name, location, interval, poll_id, sensor_name):
+    def __init__(self, hass, name, location, poll_id, sensor_name):
         """Initialize the sensor."""
-        self.hass = hass
-        self._name = name
-        self._location = location
-        self._state = STATE_UNKNOWN
-        self._poll_title = None
-        self._interval = interval
-        self._poll_id = poll_id
-        self._sensor_name = sensor_name
-        self.api = PollenAPI(hass, poll_id)
-        self._attr_icon = ICON_FLOWER_POLLEN
-        self._attr_state_class = SensorStateClass.MEASUREMENT
-        _LOGGER.debug(f"Initialized PollenSensor: {name}, {location}, {interval}, {poll_id}")
+        self.hass=hass
+        self._name=name
+        self._location=location
+        self._state=STATE_UNKNOWN
+        self._poll_title=None
+        self._poll_id=poll_id
+        self._sensor_name=sensor_name
+        self.api=PollenAPI(hass, poll_id)
+        self._attr_icon=ICON_FLOWER_POLLEN
+        self._attr_state_class=SensorStateClass.MEASUREMENT
+        self.entity_id=f"sensor.{self._sensor_name}"
+        _LOGGER.debug(f"Initialized PollenSensor: name={self._sensor_name}, poll_id={self._poll_id}")
 
-    async def async_update(self):
+    async def async_update(self, _=None):
         """Query data from API."""
         await self.api.async_update()
-        self._state = self.api.state
-        self._poll_title = self.api.poll_title
+        self._state=self.api.state
+        self._poll_title=self.api.poll_title
+        self.async_write_ha_state()
+        _LOGGER.debug(f"Update PollenSensor: name={self._sensor_name}, state={self._state}")
 
     @property
     def name(self):
@@ -90,7 +104,7 @@ class PollenSensor(SensorEntity):
     @property
     def unique_id(self):
         """Return a unique ID for the sensor."""
-        return f"{self._name}_{self._sensor_name}"
+        return self._sensor_name
 
     @property
     def state_class(self):
