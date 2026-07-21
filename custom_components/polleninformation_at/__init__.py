9dfb6b902babe5e2572ad9b07bfce6616c7d32c6
Christian Kadluba <10721825+ckadluba@users.noreply.github.com>
2026-07-21 15:35:00 +0000

    Fixed some linter errors

--- custom_components/polleninformation_at/__init__.py
+++ custom_components/polleninformation_at/__init__.py
@@ -1,3 +1,5 @@
+"""The Polleninformation.at integration component for Home Assistant."""
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
@@ -9,13 +11,13 @@ from .coordinator import PollenDataUpdateCoordinator
 CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 
-async def async_setup(hass: HomeAssistant, config: dict):
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the Polleninformation.at component."""
     hass.data.setdefault(DOMAIN, {})
     return True
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Polleninformation.at from a config entry."""
     coordinator = PollenDataUpdateCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
@@ -28,7 +30,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
